afc1ea0b997ffb35b3e10ccdda86e98b38ddcae7
Butmocks <butmocks@gmail.com>
2021-11-15 16:26:08 +0200

    js

---
+++ jsFull/intro/task1/scripts.js
@@ -0,0 +1,8 @@
+
+let c = 4;
+let d = 5;
+let e = c + d;
+
+console.log(e);
+
+document.querySelector('a').innerText = 'Hello world!';
---
+++ jsFull/variablesAndDataTypes/task1/index.js
@@ -0,0 +1,18 @@
+
+const userName = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
+
+console.log(superNumber);
+
+const personName = 'John';
+
+let age = 17;
+
+age = 77;
+
