0bec79c34e3dcf9fbacd2fa18bf030e666f51dea
butmocks <butmocks@gmail.com>
2021-12-10 02:02:56 +0200

    222

--- jsFullGit/21dom/homework/hometask9/index.js
+++ jsFullGit/21dom/homework/hometask9/index.js
@@ -1,36 +1,19 @@
+function finishList() {
+  const listElem = document.querySelector('.list');
+  const SpecialElem = document.querySelector('.special');
+  const LiNew = document.createElement('li');
+  LiNew.textContent = 1;
+  listElem.prepend(LiNew);
+  const LiBefore = document.createElement('li');
+  LiBefore.textContent = 4;
+  SpecialElem.before(LiBefore);
+  const createAfter = document.createElement('li');
+  createAfter.textContent = 6;
+  SpecialElem.after(createAfter);
+  const liElem = document.createElement('li');
+  liElem.textContent = 8;
+  listElem.append(liElem);
+}
 
 // finishList();
-// // export { finishList };
-
-// const list = {
-//   liElemFirst: document.createElement('li'),
-//   liElemFourth: document.createElement('li'),
-//   liElemSixth: document.createElement('li'),
-//   liElemEightth: document.createElement('li'),
-//   finishList() {
-//     this.liElemFirst.listElem.prepend(liElemFirst) = '1';
-//     this.liElemFourth = '4';
-//     this.liElemSixth = '6';
-//     this.liElemEightth = '8';
-//   },
-  
-// };
-
-function finishList() {
-  const liElemFirst = document.createElement('li');
-  const liElemFourth = document.createElement('li');
-  const liElemSixth = document.createElement('li');
-  const liElemEightth = document.createElement('li');
-  liElemFirst.textContent = '1';
-  liElemFourth.textContent = '4';
-  liElemSixth.textContent = '6';
-  liElemEightth.textContent = '8';
-  const listElem = document.querySelector('.list');
-  listElem.append(liElemEightth);
-  listElem.prepend(liElemFirst);
-  const specLi = document.querySelector('.special');
-  specLi.before(liElemFourth);
-  specLi.after(liElemSixth);
-}
-finishList();
-export { finishList }
+export { finishList };
