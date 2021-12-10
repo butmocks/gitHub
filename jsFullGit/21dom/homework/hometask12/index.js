c8a2caf4e76a1acdadcc921f283934a46f9cd051
butmocks <butmocks@gmail.com>
2021-12-10 03:03:21 +0200

    ahgsgtAED

---
+++ jsFullGit/21dom/homework/hometask12/index.js
@@ -0,0 +1,11 @@
+function squaredNumbers() {
+  const square = document.querySelectorAll('li[data-number]');
+  square.forEach(function (element) {
+    element.dataset.squaredNumbers = element.dataset.number * element.dataset.number;
+    console.log(element);
+  });
+}
+
+squaredNumbers();
+
+export { squaredNumbers };
---
+++ jsFullGit/21dom/homework/hometask13/index.js
@@ -0,0 +1,8 @@
+function getSection(num) {
+  const getSpan = document.querySelector(`span[data-number="${num}"]`);
+  const result = getSpan.closest('.box').dataset.section;
+  return result;
+}
+
+getSection();
+export { getSection };
