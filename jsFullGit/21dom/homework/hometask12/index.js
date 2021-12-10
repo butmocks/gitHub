af1d511b7f361db2ed4f820da876a545acde21c9
butmocks <butmocks@gmail.com>
2021-12-10 15:31:09 +0200

    dezg

--- jsFullGit/21dom/homework/hometask12/index.js
+++ jsFullGit/21dom/homework/hometask12/index.js
@@ -1,11 +1,20 @@
-
-function squaredNumbers() {
-  const square = document.querySelectorAll('.number')
-  .forEach((listItem) => {
-    const listNum = listItem.dataset.number;
-    listItem.dataset.squared = listNum * listNum;
-    // console.log(listItem)
+const squaredNumbers = () => {
+  const listItem = Array.from(document.querySelectorAll('.number')).map(el => {
+    const listNum = el.dataset.number * el.dataset.number;
+    el.dataset.squaredNumbers = listNum;
+    console.log(listNum);
   });
-}
-// squaredNumbers()
+};
 export { squaredNumbers };
+// squaredNumbers()
+
+// function squaredNumbers() {
+//   const square = document.querySelectorAll('.number')
+//   .forEach((listItem) => {
+//     const listNum = listItem.dataset.number;
+//     listItem.dataset.squared = listNum * listNum;
+//     // console.log(listItem)
+//   });
+// }
+// // squaredNumbers()
+// export { squaredNumbers };
