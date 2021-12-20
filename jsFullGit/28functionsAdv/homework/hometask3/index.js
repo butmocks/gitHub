1875093e2aa328dd7ecd47964405a859af346183
butmocks <butmocks@gmail.com>
2021-12-20 03:47:04 +0200

    df

--- jsFullGit/28functionsAdv/homework/hometask3/index.js
+++ jsFullGit/28functionsAdv/homework/hometask3/index.js
@@ -1,7 +1,7 @@
 export const mult = a => b => a * b;
 
-export const twice = mult(2);
-export const triple = mult(3);
+export const twice = mult(7);
+export const triple = mult(9);
 
-console.log(mult(2)(3));
-console.log(mult(10)(3));
+console.log(mult(5)(4));
+console.log(mult(7)(9));
