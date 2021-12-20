1c0fe568bc3e8e614d0b5ff315ba0fbf6a1a2892
butmocks <butmocks@gmail.com>
2021-12-20 02:37:38 +0200

    27,2

--- jsFullGit/27browserStorage/homework/hometask1/index.js
+++ jsFullGit/27browserStorage/homework/hometask1/index.js
@@ -1,16 +1,8 @@
-export { getLocalStorageData }
-
-localStorage.clear();
-
-localStorage.setItem('hobbies', JSON.stringify({ name: 'Tom' }));
-localStorage.setItem('name', 'Tom');
-localStorage.getItem('age', JSON.stringify(17));
-
 const getLocalStorageData = () => {
   return Object.entries(localStorage).reduce((acc, [key, value]) => {
     let newValue;
     try {
-      const newValue = JSON.parse(value);
+      newValue = JSON.parse(value);
     } catch (e) {
       newValue = value;
     }
@@ -21,4 +13,4 @@ const getLocalStorageData = () => {
   }, {});
 };
 
-console.log(getLocalStorageData());
+export { getLocalStorageData };
