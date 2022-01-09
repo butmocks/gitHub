beb6ee34c8199df0041e6194327f906098ea3c99
butmocks <butmocks@gmail.com>
2022-01-09 04:28:08 +0200

    d

--- jsFullGit/33restApiConnect/homework/hometask2/index.js
+++ jsFullGit/33restApiConnect/homework/hometask2/index.js
@@ -4,12 +4,3 @@ export const getTasksList = () => fetch(`${baseUrl}`).then(response => response.
 
 export const getTaskById = taskId =>
   fetch(`${baseUrl}/${taskId}`).then(response => response.json());
-
-// examples
-getTasksList().then(tasksList => {
-  console.log(tasksList); // array of the task objects - [ {'id':'1', 'done':false ... }, {'id':'2', 'done':true ... }, ...]
-});
-
-getTaskById('2').then(taskData => {
-  console.log(taskData); // {'id':'2', 'done':true ... }
-});
