a7eddcb426461ec020a0e63fe4b4aa3b1ee960c9
butmocks <butmocks@gmail.com>
2022-01-09 18:37:58 +0200

    zdfh

---
+++ jsFullGit/35errorHandlingHttp/homework/hometask3/index.js
@@ -0,0 +1,32 @@
+import { renderUserData } from './user.js';
+import { renderRepos, cleanReposList } from './repos.js';
+import { fetchUserData, fetchRepositories } from './gateways.js';
+import { showSpinner, hideSpinner } from './spinner.js';
+
+const defaultUser = {
+  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+  name: '',
+  location: '',
+};
+
+renderUserData(defaultUser);
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+const onSearchUser = () => {
+  cleanReposList();
+  showSpinner();
+  const userName = userNameInputElem.value;
+  fetchUserData(userName)
+    .then(userData => {
+      renderUserData(userData);
+      return userData.repos_url;
+    })
+    .then(url => fetchRepositories(url))
+    .then(reposlist => renderRepos(reposlist))
+    .catch(error => alert(error.message))
+    .finally(() => hideSpinner());
+};
+
+showUserBtnElem.addEventListener('click', onSearchUser);
