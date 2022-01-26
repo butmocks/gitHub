c8becf69dd58cb8207f72bda631350f554a5bd46
Yaroslav Platonov <butmocks@gmail.com>
2022-01-26 12:26:24 +0200

    lc

---
+++ liveCodding/react/26.01/src/Auth.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Login from './Login';
+import Logout from './Logout';
+import Spinner from './Spinner';
+
+// algo
+// 1. show Login by default
+// 2. after login show Spinner for 2 secs
+// 3. after 2 secs - show logout
+// 4. after Logout click - show login
+
+class Auth extends React.Component {
+  // constructor(props) {
+
+  //   this.state = {
+  //     isLoggedIn: false,
+  //     isProcessing: false
+  //   }
+  // }
+
+  handleLoginClick = () => {
+    // input: obj
+    // output: undef
+    this.setState({
+      isLoadding: true,
+    });
+
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+        isLoggedIn: true,
+      });
+    }, 2000)
+  };
+
+
+  state = {
+    isLoggedIn: false,
+    isProcessing: false
+  }
+
+render() {
+  const {isLoggedIn, isLoading } = this.state;
+
+  if(isLoading) {
+    return <Spinner />;
+  }
+
+  if(isLoggedIn) {
+    return <logout />
+  }
+
+
+  return <Login onLogin={this.handleLoginClick} />
+};
+
+export default Auth;
---
+++ liveCodding/react/26.01/src/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// input: 
+// output: 
+
+const Login = props => {
+  return (
+    <button className="login btn" onLogin={this.handleLogin}>
+      Login
+    </button>
+  );
+}
+
+export default Login;
